8a636c9172c152e15a58354d7bc8325415240cfa
Max Calabrese <max.calabrese@ymail.com>
2015-05-09 23:44:46 +0200

    Create and update Users from oauth data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :nickname, :email, case_sensitive: false
   validates_uniqueness_of :flickr_uid
+
+  # @return [User]
+  # @param [Hash] auth_hash
+  def self.create_from_omniauth(auth_hash)
+    create(attrs_from_omniauth(auth_hash))
+  end
+
+  protected
+  # @notice that the attributes are copied instead of sliced to avoid issues with mass assignment protection.
+  def self.attrs_from_omniauth(auth_hash)
+    {
+        name: auth_hash[:info][:name],
+        nickname: auth_hash[:info][:nickname],
+        email: auth_hash[:info][:email],
+        flickr_uid: auth_hash[:uid]
+    }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
+require 'support/omniauth'
 
 RSpec.describe User, type: :model do
-  it { should validate_uniqueness_of :email, case_sensitive: false }
+  it { should validate_uniqueness_of :email }
   it { should validate_uniqueness_of :flickr_uid }
   it { should validate_uniqueness_of :nickname }
+
+  describe '.create_from_omniauth' do
+    subject { User.create_from_omniauth(mock_auth_hash) }
+    its(:email) { should eq 'joe@bloggs.com' }
+    its(:flickr_uid) { should eq '1234567' }
+    its(:name) { should eq 'Joe Bloggs' }
+    its(:nickname) { should eq 'jbloggs' }
+  end
 end
