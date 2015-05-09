d37c7e5b7b11ec686be06a9fa1e2b1f72e5d56c3
Max Calabrese <max.calabrese@ymail.com>
2015-05-09 22:29:11 +0200

    fire controller / action load/unload events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,39 @@
 //= require foundation
 //= require turbolinks
 //= require_tree .
+'use strict';
 
-$(function(){ $(document).foundation(); });
+$(function(){
+    $doc.foundation();
+});
+/**
+ * Fire custom events when Turbolinks loads and unloads pages
+ * These events are "namespaced" to the controller and action and can be used to add controller specific functionality.
+ *
+ * So if the request is for Users#Show this will fire:
+ *
+ *   portfolio.myapp.show:change
+ *   portfolio.myapp:change
+ *   portfolio.myapp:after-remove
+ *   portfolio.myapp.show:after-remove
+ *
+ * Note that the controller name is the last part of the controller’s name, underscored, without the ending Controller
+ * @see http://apidock.com/rails/ActionController/Metal/controller_name/class
+ * @see https://github.com/rails/turbolinks
+ *
+ */
+(function($doc){
+    var add_triggers = function(event){
+        var $b = $('body');
+        var namespaces = ['myapp', $b.data('controller')];
+        $doc.trigger(namespaces.join('.') + event);
+        namespaces.push($b.data('action'));
+        $doc.trigger(namespaces.join('.') + event);
+    };
+    $doc.on('page:change', function(){
+        add_triggers(':change');
+    });
+    $doc.on('page:after-remove', function(){
+        add_triggers(':after-remove');
+    });
+}($(document)));
