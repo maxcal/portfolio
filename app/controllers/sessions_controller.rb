caf0cdb6d274a7884f3b0189e17a05197849e609
Max Calabrese <max.calabrese@ymail.com>
2015-05-10 14:38:08 +0200

    Create an action to redirect unauthorized users to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,11 @@ class SessionsController < ApplicationController
     request.env['warden'].logout
     redirect_to root_path, notice: t('sessions.flash.you_have_been_signed_out')
   end
+
+  def new
+    if signed_in?
+      redirect_to :root
+    end
+  end
+
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,17 +3,32 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
 
   subject { response }
+  let(:warden) { double('warden') }
+  before { request.env['warden'] = warden }
 
-  describe "DELETE #destroy" do
-
-    before do
-      # Stub out warden.
-      warden = double('warden')
+  describe 'DELETE #destroy' do
+    before do |example|
+      allow(warden).to receive(:logout)
+      delete :destroy unless example.metadata[:skip_request]
+    end
+    it { should redirect_to :root }
+    it 'signs out user', skip_request: true do
       expect(warden).to receive(:logout)
-      request.env['warden'] = warden
       delete :destroy
     end
+  end
 
-    it { should redirect_to :root }
+  describe 'GET new' do
+    before do |example|
+      allow(warden).to receive(:user)
+      allow(controller).to receive(:signed_in?).and_return(example.metadata[:signed_in] || false)
+      get :new
+    end
+    context 'without a signed in user' do
+      it { should render_template :new }
+    end
+    context 'with a signed in user', signed_in: true do
+      it { should redirect_to :root }
+    end
   end
 end
