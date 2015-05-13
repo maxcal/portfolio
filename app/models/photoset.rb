0b580082de31e464788c15a47d5f7928e5eda1d1
Max Calabrese <max.calabrese@ymail.com>
2015-05-13 18:31:51 +0200

    add method to import photosets

--- app/models/photoset.rb
+++ app/models/photoset.rb
@@ -2,4 +2,23 @@ class Photoset < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :flickr_uid
   validates_uniqueness_of :title
+
+
+  # Imports photosets from `flickr.photosets.getList`.
+  # The user_id params is required - any additional hash arguments will be forwarded to
+  # @param [String] user_id
+  # @param [Hash] kwargs
+  # @yield [photoset, raw_data]
+  #   @yieldreturn [Photoset]
+  def self.import user_id:, **kwargs, &block
+    result = flickr.photosets.getList(user_id: user_id)
+    photosets = result.map do |raw|
+      set = create_with(
+          title: raw["title"],
+          description: raw["description"]
+      ).find_or_initialize_by(flickr_uid: raw["id"])
+      yield(set, raw) if block_given?
+      set
+    end
+  end
 end
--- spec/models/photoset_spec.rb
+++ spec/models/photoset_spec.rb
@@ -3,4 +3,36 @@ require 'rails_helper'
 RSpec.describe Photoset, type: :model do
   it { should validate_uniqueness_of :flickr_uid }
   it { should validate_uniqueness_of :title }
+
+  describe '.import' do
+    let(:photosets) do
+      sets = nil
+      VCR.use_cassette('photosets_import') do
+        sets = Photoset.import(user_id: '62829091@N05')
+      end
+      sets
+    end
+    it "gets photosets" do
+      expect(photosets.first).to be_a Photoset
+    end
+    it "sets flickr_uid" do
+      expect(photosets.first.flickr_uid).to eq '72157647753138397'
+    end
+    it "sets title" do
+      expect(photosets.first.title).to eq 'Showcase'
+    end
+    it "sets the description" do
+      expect(photosets.first.description).to eq 'Test description'
+    end
+
+    it "yields the results and raw data to a block" do
+      sets = nil
+      VCR.use_cassette('photosets_import') do
+        sets = Photoset.import(user_id: '62829091@N05') do |set, raw|
+          set.title = raw["title"].reverse
+        end
+      end
+      expect(sets.first.title).to eq 'Showcase'.reverse
+    end
+  end
 end
