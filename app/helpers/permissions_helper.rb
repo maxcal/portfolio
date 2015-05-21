b9bc40029f88038f306568d3d3a5cfaec773bdd0
Max Calabrese <max.calabrese@ymail.com>
2015-05-21 15:50:28 +0200

    create helper to dry out action links

---
+++ app/helpers/permissions_helper.rb
@@ -0,0 +1,40 @@
+module PermissionsHelper
+  # Generates a hash of links to edit or destroy a resource
+  # @param [Instance|Class] resource
+  # @param [Actions] (optional) a list of crud actions to create links to.
+  #   defaults to
+  # @param [Hash] kwargs optional hash to pass to link to
+  # @option kwargs [String] :controller - controller name to use.
+  #   Otherwise a guess is performed based on the resource class name.
+  # @option kwargs [Hash] url_extras - passed to url_for. Can be used for nested resources.
+  # @return [Hash] a list of links to actions which the user is allowed to perform
+  def crud_links_for_resource(resource,  actions = nil, **kwargs)
+    i18n_key = resource.model_name.i18n_key
+    controller = kwargs[:controller] || i18n_key.to_s.pluralize
+    url_extras = kwargs[:url_extras] || {}
+    unless actions
+      if resource.is_a? Class
+        actions = [:new]
+      else
+        actions = [:edit, :destroy]
+      end
+    end
+    actions.keep_if { |action| can? action, resource }.each_with_object({}) do |action, hash|
+      url_params = { action: action, controller: controller, id: resource }.merge(url_extras)
+      txt = t("#{ i18n_key }.#{action}")
+      if resource.is_a? Class
+        url_params.except!(:id)
+      end
+      options = kwargs.except(:controller, :url_extras)
+      case action
+        when :destroy
+          options.merge!(
+              method: :delete,
+              data: { confirm: t("#{ i18n_key }.confirm_#{action}") }.merge(options[:data].to_h)
+          )
+        else
+      end
+      hash[action] = link_to(txt,url_params,options)
+    end
+  end
+end
---
+++ spec/helpers/permissions_helper_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the PermissionsHelper. For example:
+#
+# describe PermissionsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe PermissionsHelper, type: :helper do
+
+  let(:resource) { build_stubbed(:photoset) }
+  let(:ability) do
+    Object.new.extend(CanCan::Ability)
+  end
+
+  before { allow(controller).to receive('current_ability').and_return(ability) }
+  describe '.crud_links_for_resource' do
+    let(:buttons) { |ex|
+      helper.crud_links_for_resource(resource, data: { foo: 'bar' }).values
+    }
+    let(:node) { Capybara::Node::Simple.new(buttons.join) }
+    subject { node }
+
+    describe 'destroy link' do
+      context 'when authorized' do
+        before do
+          ability.can :destroy, resource
+        end
+        let(:link) { node.find_link(I18n.t('photoset.destroy')) }
+        it 'has the correct method' do
+          expect(link["data-method"]).to eq "delete"
+        end
+        it 'has the correct url' do
+          expect(link[:href]).to eq url_for(resource)
+        end
+        it "accepts a hash of options to pass to link_to" do
+          expect(link['data-foo']).to eq 'bar'
+        end
+        it 'has a confirmation' do
+          expect(link["data-confirm"]).to eq I18n.t('photoset.confirm_destroy')
+        end
+      end
+      context 'when unauthorized' do
+        it { should_not have_link I18n.t('photoset.destroy') }
+      end
+    end
+
+    describe 'destroy link' do
+      context 'when authorized' do
+        before do
+          ability.can :edit, resource
+        end
+        let(:link) { node.find_link(I18n.t('photoset.edit')) }
+        it 'has the correct url' do
+          expect(link[:href]).to eq edit_photoset_path(resource)
+        end
+      end
+      context 'when unauthorized' do
+        it { should_not have_link I18n.t('photoset.edit') }
+      end
+    end
+
+    describe 'new link' do
+      let(:resource) { Photoset }
+      let(:link) { node.find_link(I18n.t('photoset.new')) }
+      before do
+        ability.can :create, resource
+      end
+      it 'has the correct url' do
+        expect(link[:href]).to eq new_photoset_path
+      end
+    end
+
+  end
+end
